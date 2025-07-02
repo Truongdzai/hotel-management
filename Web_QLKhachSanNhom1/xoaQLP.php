24f123197e2df0e3622d285be1a743faa1afb1c5
Truongdzai <qvantruong205@gmail.com>
2025-07-03 06:30:58 +0700

    🎉 Initial commit: Upload web quản lý khách sạn

---
+++ Web_QLKhachSanNhom1/xoaQLP.php
@@ -0,0 +1,15 @@
+
+<?php
+session_start();
+include("ketnoi.php");
+
+    $user_xoa=$_REQUEST["user"];
+    $sql="delete from phong where ma_phong='".$user_xoa."'";
+    $kq=mysqli_query($conn, $sql) or die ("Không thể xuất thông tin".mysqli_error($conn));
+    echo ("<script language=javascript>
+    {
+    alert('Xóa phòng thành công');
+    window.location='QLP.php';}
+    </script> ");
+?>
+
